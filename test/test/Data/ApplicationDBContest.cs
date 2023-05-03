aca22c6444eb9098b3e2be37fecd7d76344d7c52
Dheeraj06384004 <116487923+Dheeraj06384004@users.noreply.github.com>
2023-05-03 11:21:15 +0530

    Add files via upload

---
+++ test/test/Data/ApplicationDBContest.cs
@@ -0,0 +1,14 @@
+﻿using AWSDBConnect.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace test.Data
+{
+    public class ApplicationDBContest : DbContext
+    {
+        public ApplicationDBContest(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Webdbtemp> Webdbtemp { get; set; }
+    }   
+}
